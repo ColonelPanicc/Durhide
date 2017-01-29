946d78571b807652b99ad3f7ea8ec8685a274ae7
Oliver McLeod <olivermcleod96@gmail.com>
2017-01-29 00:16:27 +0000

    settings actually work now

--- AndroidApp/app/src/main/java/co/brookesoftware/mike/smilingpooemoji/SettingsActivity.java
+++ AndroidApp/app/src/main/java/co/brookesoftware/mike/smilingpooemoji/SettingsActivity.java
@@ -1,37 +1,18 @@
 package co.brookesoftware.mike.smilingpooemoji;
 
-import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        setContentView(R.layout.settings_main);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
-
-    @Override
-    public void onPostCreate(Bundle savedInstanceState){
-        super.onPostCreate(savedInstanceState);
-
-        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.N) {
-            LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent();
-            View bar = LayoutInflater.from(this).inflate(R.layout.settings_bar, root, false);
-            root.addView(bar, 0);
-        }else{
-            FrameLayout root = (FrameLayout) findViewById(android.R.id.list).getParent();
-            View bar = LayoutInflater.from(this).inflate(R.layout.settings_bar, root, false);
-            root.addView(bar, 0);
-        }
-    }
-
 
 }
