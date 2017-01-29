641bebabc4ccb2771448c690ddd0a5432b05802b
Oliver McLeod <olivermcleod96@gmail.com>
2017-01-29 09:21:42 +0000

    hopefully unlocking changing setting chiev

--- AndroidApp/app/src/main/java/co/brookesoftware/mike/smilingpooemoji/SettingsFragment.java
+++ AndroidApp/app/src/main/java/co/brookesoftware/mike/smilingpooemoji/SettingsFragment.java
@@ -1,7 +1,11 @@
 package co.brookesoftware.mike.smilingpooemoji;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+
+import com.google.android.gms.games.Games;
 
 /**
  * Created by olivermcleod on 28/01/2017.
@@ -13,6 +17,21 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        SwitchPreference showCones = (SwitchPreference) findPreference(getString(R.string.preference_key_show_cones));
+        showCones.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                if(MainActivity.mGoogleApiClient != null && MainActivity.mGoogleApiClient.isConnected()){
+                    Games.Achievements.unlock(MainActivity.mGoogleApiClient,getString(R.string.achievement_change_a_setting));
+                }
+                return false;
+            }
+        });
     }
 
 }
