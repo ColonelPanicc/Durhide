9d7b7a7776d7680987f7926647127ead2adaf1df
Ryan Collins <OhmGeek@users.noreply.github.com>
2017-02-23 18:06:11 +0000

    Update urls.py

--- backend/durhack/durhack/urls.py
+++ backend/durhack/durhack/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^',include('webclient.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api/cameras/', include('cameras.urls')),
 ]
